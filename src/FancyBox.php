a9abdfe5ba09a942d802ea695efd0d1f6beafa9c
Paul P <bigpaulie25ro@yahoo.com>
2015-04-30 13:40:48 +0300

    Adding content type related css classes

--- src/FancyBox.php
+++ src/FancyBox.php
@@ -18,18 +18,25 @@ class FancyBox extends Widget {
     public function run() {
         if ($this->type == "image") {
             // create anchor tag & image tag
+            $class = $this->htmlOptions['linkOptions']['class'];
+            $this->htmlOptions['linkOptions']['class'] = 'fancybox.image ' . $class;
             echo Html::a(Html::img($this->item['src'], $this->htmlOptions['imageOptions']) , $this->item['href'] , $this->htmlOptions['linkOptions']);
         } elseif ($this->type == "inline") {
+            $class = $this->htmlOptions['linkOptions']['class'];
+            $this->htmlOptions['linkOptions']['class'] = 'fancybox.inline ' . $class;
             echo Html::a($this->item['text'], $this->item['href'], $this->htmlOptions['linkOptions']);
         } elseif ($this->type == "iframe") {
             /**
              * Add class iframe to the link 
              */
-            $class = 'iframe ' . $this->htmlOptions['linkOptions']['class'];
-            $this->htmlOptions['linkOptions']['css'] = $class;
+            $class = $this->htmlOptions['linkOptions']['class'];
+            $this->htmlOptions['linkOptions']['class'] = 'fancybox.iframe ' . $class;
 
             echo Html::a($this->item['text'], $this->item['href'], $this->htmlOptions['linkOptions']);
         } elseif ($this->type == "ajax") {
+            $class = $this->htmlOptions['linkOptions']['class'];
+            $this->htmlOptions['linkOptions']['class'] = 'fancybox.ajax ' . $class;
+            
             echo Html::a($this->item['text'] , $this->item['href'] , $this->htmlOptions['linkOptions']);
         }
     }
