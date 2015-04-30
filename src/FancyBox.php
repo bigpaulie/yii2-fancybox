8fe1e3af5e9c5295aa5bbc626274bb91d6c9923f
Paul P <bigpaulie25ro@yahoo.com>
2015-04-30 12:51:07 +0300

    Bug fix  Invalid argument supplied for foreach() #1

--- src/FancyBox.php
+++ src/FancyBox.php
@@ -18,9 +18,7 @@ class FancyBox extends Widget {
     public function run() {
         if ($this->type == "image") {
             // create anchor tag & image tag
-            echo Html::beginTag('a', $this->item['href'], $this->htmlOptions['linkOptions']);
-            echo Html::img($this->item['src'], $this->htmlOptions['imageOptions']);
-            echo Html::endTag('a');
+            echo Html::a(Html::img($this->item['src'], $this->htmlOptions['imageOptions']) , $this->item['href'] , $this->htmlOptions['linkOptions']);
         } elseif ($this->type == "inline") {
             echo Html::a($this->item['text'], $this->item['href'], $this->htmlOptions['linkOptions']);
         } elseif ($this->type == "iframe") {
